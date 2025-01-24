78e9ee4877e9b42b1404c21397076284a5b46a82
lorenliu <lorensuyan@icloud.com>
2025-01-23 22:29:41 -0800

    fuck all examples

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,23 +8,13 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the Main.java file in the project.
- */
+
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  
   public Robot() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
   }
 
@@ -54,12 +44,7 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
+   
   }
 
   /** This function is called periodically during autonomous. */
@@ -72,9 +57,9 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+    // if (m_autonomousCommand != null) {
+    //   m_autonomousCommand.cancel();
+    // }
   }
 
   /** This function is called periodically during operator control. */
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,8 +10,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Constants.OIConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -21,70 +19,19 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 
 public class RobotContainer {
-  // private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  // private final SwerveSubsystem m_robotDrive = new SwerveSubsystem();
-  // private final XboxController m_driverController = new XboxController(OperatorConstants.kDriverControllerPort);
-  // private final CommandXboxController m_driverControllerCommand = new CommandXboxController(OperatorConstants.kDriverControllerPort);
-  private final CommandXboxController m_driverController = new CommandXboxController(0);
-  private final SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem(m_driverController);
-  
+  CommandXboxController driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
 
-  
-  
-
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  //   m_robotDrive.setDefaultCommand(
-  //   // The left stick controls translation of the robot.
-  //   // Turning is controlled by the X axis of the right stick.
-
-  //   //square button for aim assist
-  //   //uncomment if no worky
-  //   new RunCommand(
-  //       () -> m_robotDrive.DriverDrive(
-  //           -MathUtil.applyDeadband(m_driverControllerCommand.getLeftY(), OIConstants.kDriveDeadband),
-  //           -MathUtil.applyDeadband(m_driverControllerCommand.getLeftX(), OIConstants.kDriveDeadband),
-  //           -MathUtil.applyDeadband(m_driverControllerCommand.getRightX(), OIConstants.kDriveDeadband),
-  //           true, true, m_driverController),
-  //       m_robotDrive));
-  m_swerveSubsystem.setDefaultCommand(
-    new RunCommand(
-        () -> m_swerveSubsystem.drive(
-            -m_driverController.getLeftY(),
-            m_driverController.getLeftX(),
-            m_driverController.getRightX()
-            // true,  // fieldRelative (change as needed)
-            // false  // rateLimit (change as needed)
-        ),
-        m_swerveSubsystem
-    ));
-}
-
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    
+    driverController.a().onTrue(new RunCommand(() -> {
+      System.out.println("A button pressed");
+    }));
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
+  
+  private void configureBindings() {}
 
-    //return nothing
+ 
+  public Command getAutonomousCommand() {
     return null;
   }
 }
