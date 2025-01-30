b8a6a1967daa3532d9e0c13ce69996da457237ad
lorenliu <lorensuyan@icloud.com>
2025-01-29 18:59:28 -0800

    1

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -80,11 +80,10 @@ public class SwerveModule {
     }
 
     public SwerveModuleState setDesiredState(SwerveModuleState moduleDesiredState){
-        System.out.println("passed in speed: "+moduleDesiredState.speedMetersPerSecond);
         m_moduleDesiredState = moduleDesiredState;
         m_moduleDesiredState.optimize(moduleDesiredState.angle);
 
-        System.out.println("optimized speed: "+m_moduleDesiredState.speedMetersPerSecond);
+        //System.out.println("optimized speed: "+m_moduleDesiredState.speedMetersPerSecond);
         //m_drivingKraken.setControl(m_velocityDutyCycle.withVelocity(m_moduleDesiredState.speedMetersPerSecond * ModuleConstants.kDrivingEncoderVelocityFactor));
         m_drivingKraken.setControl(m_velocityDutyCycle.withVelocity(m_moduleDesiredState.speedMetersPerSecond));
         m_turningFalcon.setControl(m_velocityDutyCycle.withVelocity(m_moduleDesiredState.angle.getRadians() * ModuleConstants.kTurningEncoderVelocityFactor));
